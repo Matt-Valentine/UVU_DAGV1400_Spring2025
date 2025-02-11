89b3db1623017968de21168c1ded2157cf15f943
Matthew Valentine <mattvalentine19@gmail.com>
2025-02-10 22:14:19 -0700

    Finished Scripting2 Challenge

--- AdventureGame/AdventureGame/Assets/Scenes/Scripts/Scripting2Lab/TransformController.cs
+++ AdventureGame/AdventureGame/Assets/Scenes/Scripts/Scripting2Lab/TransformController.cs
@@ -13,10 +13,11 @@ public class TransformController : MonoBehaviour
     {
         // Move the Target GameObject
         var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        var y = Mathf.PingPong(Time.time, 6);
+        var p = new Vector3(x, y, 0);
         transform.position = p;
         
         //Rotate Target GameObject
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(30, 60, 0) * Time.deltaTime);
     }
 }
